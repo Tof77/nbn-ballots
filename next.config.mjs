221f70e28b2878525cda1943ff827412d4fd3c13
Christophe China <50609637+Tof77@users.noreply.github.com>
2025-04-16 08:44:19 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configuration simplifiée
   experimental: {
     serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
   },
-  // Ajout de la configuration pour le dossier src
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  // Forcer l'inclusion de tous les dossiers API
+  output: 'standalone',
+  outputFileTracing: true,
+  outputFileTracingRoot: './',
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/@esbuild/linux-x64',
+    ],
+  },
+  outputFileTracingIncludes: {
+    '*': ['app/api/**/*'],
+  },
 }
 
 export default nextConfig
